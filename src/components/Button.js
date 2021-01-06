3fcff0779598f1cb72c64eabe311f718152ad7dd
Chase Wickett <wickettc@yahoo.com>
2021-01-05 22:02:42 -0500

    working commit, still needs overflow logic and some styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import '../css/button.css';
 
-const Button = ({ char, handleButton }) => {
+const Button = ({ char, handleButton, disabled }) => {
     const ident = typeof char === 'number' ? 'num' : 'operator';
     return (
         <button
             data-type={ident}
             data-char={char}
             className="button"
+            disabled={disabled}
             onClick={(e) => handleButton(e)}
         >
             {char}
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,15 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Button from './Button';
 import ResultScreen from './ResultScreen';
 import CalcScreen from './CalcScreen';
 import '../css/calculator.css';
+import { create, all } from 'mathjs';
+
+const math = create(all);
 
 const Calculator = () => {
     const [result, setResult] = useState(0);
-    const [calc, setCalc] = useState(0);
+    const [calc, setCalc] = useState('');
+    const [decimal, setDecimal] = useState(false);
 
     const handleButton = (e) => {
-        if (e.target.dataset.type === 'num') {
+        if (e.target.dataset.char === '=') {
+            handleEquals();
+        } else if (e.target.dataset.char === 'C') {
+            handleClear();
+        } else if (e.target.dataset.char === '<-') {
+            handleBackSpace(e);
+        } else if (e.target.dataset.char === '.') {
+            handleDecimal(e);
+        } else if (e.target.dataset.type === 'num') {
             concatNums(e);
         } else if (e.target.dataset.type === 'operator') {
             stringNumsAndOperator(e);
@@ -25,13 +37,44 @@ const Calculator = () => {
     };
 
     const stringNumsAndOperator = (e) => {
-        if (calc === 0) {
-            setCalc(`${parseInt(result)} ${e.target.dataset.char}`);
+        if (calc === '') {
+            setCalc(`${result} ${e.target.dataset.char}`);
             setResult(0);
+            setDecimal(false);
         } else {
             setCalc(`${calc} ${result} ${e.target.dataset.char}`);
             setResult(0);
+            setDecimal(false);
         }
+    };
+
+    const handleBackSpace = () => {
+        //flip decimal btn back on if decimal is backspaced
+        if (result[result.length - 1] === '.') {
+            setDecimal(false);
+        }
+        if (result.length === 1) {
+            setResult(0);
+        } else {
+            setResult(result.slice(0, result.length - 1));
+        }
+    };
+
+    const handleDecimal = (e) => {
+        setResult(result + e.target.dataset.char);
+        setDecimal(true);
+    };
+
+    const handleClear = () => {
+        setResult(0);
+        setDecimal(false);
+        setCalc('');
+    };
+
+    const handleEquals = () => {
+        setResult(math.evaluate(`${calc} ${result}`));
+        setCalc('');
+        setDecimal(false);
     };
 
     return (
@@ -39,24 +82,60 @@ const Calculator = () => {
             <ResultScreen result={result} />
             <CalcScreen calc={calc} />
             <div className="btn-container">
-                <Button handleButton={handleButton} char={9} />
-                <Button handleButton={handleButton} char={8} />
-                <Button handleButton={handleButton} char={7} />
-                <Button handleButton={handleButton} char={6} />
-                <Button handleButton={handleButton} char={5} />
-                <Button handleButton={handleButton} char={4} />
-                <Button handleButton={handleButton} char={3} />
-                <Button handleButton={handleButton} char={2} />
-                <Button handleButton={handleButton} char={1} />
-                <Button handleButton={handleButton} char={0} />
-                <Button handleButton={handleButton} char={'/'} />
-                <Button handleButton={handleButton} char={'*'} />
-                <Button handleButton={handleButton} char={'-'} />
-                <Button handleButton={handleButton} char={'+'} />
-                <Button handleButton={handleButton} char={'='} />
-                <Button handleButton={handleButton} char={'.'} />
-                <Button handleButton={handleButton} char={'C'} />
-                <Button handleButton={handleButton} char={'<-'} />
+                <Button handleButton={handleButton} char={9} disabled={false} />
+                <Button handleButton={handleButton} char={8} disabled={false} />
+                <Button handleButton={handleButton} char={7} disabled={false} />
+                <Button handleButton={handleButton} char={6} disabled={false} />
+                <Button handleButton={handleButton} char={5} disabled={false} />
+                <Button handleButton={handleButton} char={4} disabled={false} />
+                <Button handleButton={handleButton} char={3} disabled={false} />
+                <Button handleButton={handleButton} char={2} disabled={false} />
+                <Button handleButton={handleButton} char={1} disabled={false} />
+                <Button
+                    handleButton={handleButton}
+                    char={0}
+                    disabled={result === 0 ? true : false}
+                />
+                <Button
+                    handleButton={handleButton}
+                    char={'/'}
+                    disabled={result === 0 && calc === '' ? true : false}
+                />
+                <Button
+                    handleButton={handleButton}
+                    char={'*'}
+                    disabled={result === 0 && calc === '' ? true : false}
+                />
+                <Button
+                    handleButton={handleButton}
+                    char={'-'}
+                    disabled={result === 0 && calc === '' ? true : false}
+                />
+                <Button
+                    handleButton={handleButton}
+                    char={'+'}
+                    disabled={result === 0 && calc === '' ? true : false}
+                />
+                <Button
+                    handleButton={handleButton}
+                    char={'='}
+                    disabled={calc === '' ? true : false}
+                />
+                <Button
+                    handleButton={handleButton}
+                    char={'.'}
+                    disabled={decimal}
+                />
+                <Button
+                    handleButton={handleButton}
+                    char={'C'}
+                    disabled={result === 0 && calc === '' ? true : false}
+                />
+                <Button
+                    handleButton={handleButton}
+                    char={'<-'}
+                    disabled={result === 0 ? true : false}
+                />
             </div>
         </div>
     );
